415a272dc5a7958e1ab27364edcd45878fbfc68c
Justin Moroz <justin.moroz@finteractions.com>
2021-12-17 08:08:00 -0500

    fix: remove unused stylesheets

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { RecoilRoot } from 'recoil';
 import NavBar from './components/Navbar';
 import reportWebVitals from './reportWebVitals';
