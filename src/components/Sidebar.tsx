2247fc2f51e8f2f34106f56ee1973344d2f5c1c5
Patrick Brennan <pbrennanmusic@gmail.com>
2022-01-31 10:59:06 -0500

    fix: Sidebar height. Need to fix collapse.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,30 +2,30 @@ import React from 'react';
 import {
   Nav,
   Navbar,
-  Container,
+  // Container,
 } from 'react-bootstrap';
 import icon from '../images/zk_favicon.png';
+import style from '../styles/sidebar';
 
 function Sidebar() {
   return (
-    <div className="float-left">
-      <Navbar collapseOnSelect expand="md" bg="info" variant="dark">
-        <Container>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav">
-            <img
-              src={icon}
-              width="30px"
-              alt="ZKL"
-            />
-          </Navbar.Toggle>
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="flex-column">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+    <div style={style.sidebar}>
+      <Navbar expand="md" variant="dark">
+        {/* <Container> */}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav">
+          <img
+            src={icon}
+            width="30px"
+            alt="ZKL"
+          />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="flex-column">
+            <Nav.Link href="#nfts">NFTs</Nav.Link>
+            <Nav.Link href="#ipfs">IPFS</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        {/* </Container> */}
       </Navbar>
     </div>
   );
--- src/styles/sidebar.ts
+++ src/styles/sidebar.ts
@@ -1,5 +1,15 @@
-export default {
+const style = {
   sidebar: {
-    backgroundColor: '',
+    position: 'fixed',
+    backgroundColor: '#0784A3',
+    top: '0',
+    bottom: '0',
+    left: '0',
+    maxHeight: '100vh !important',
+    zIndex: '100',
+    padding: '48px 0 0',
+    boxShadow: 'inset -1px 0 0 rgba(0, 0, 0, .1)',
   },
-};
+} as const;
+
+export default style;
