02665f00793f2652531aeea4076f40ab9ef6dd5c
Justin Moroz <justin.moroz@finteractions.com>
2022-03-03 14:39:23 -0500

    feat: use css modules in member dashboard components

--- src/components/memberDashboard/MyProjects.tsx
+++ src/components/memberDashboard/MyProjects.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import {
   Container, Row, Col, Button,
 } from 'react-bootstrap';
-
 import { Plus } from 'react-bootstrap-icons';
+import style from '../../styles/memberDashboard.module.css';
 
 function MyProjects() {
   return (
     <Container>
-      <div className="my-projects-wrapper">
-        <p className="my-projects-title">MY PROJECTS</p>
+      <div className={style['my-projects-wrapper']}>
+        <p className={style['my-projects-title']}>MY PROJECTS</p>
         <Row>
           <Col xs={4}>
-            <div className="dashboard-create-project">
-              <Button className="new-project-button">
+            <div className={style['dashboard-create-project']}>
+              <Button className={style['new-project-button']}>
                 <Plus style={{ verticalAlign: 'bottom' }} size={24} />
                 NEW PROJECT
               </Button>
